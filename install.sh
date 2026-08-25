11bae0d4b090d471e21d86ffd54195c2641bd643
Manuel Carrer <manuel.carrer@zohomail.eu>
2026-08-25 14:00:18 +0200

    Update install

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+
+set -e
+
 module load uv
-module load cuda/12.8
+module load cuda/13.0
 module load python3/3.12.11
 
-cat >>"${HOME}/.bashrc" <<EOF
-export UV_CACHE_DIR=$PERM/.cache/uv
-EOF
+if [[ "${UV_CACHE_DIR@a}" != *x* ]]; then
+    echo "Setting UV_CACHE_DIR in ~/.bashrc"
+    echo "export UV_CACHE_DIR=$PERM/.cache/uv" >>"${HOME}"/.bashrc
+    source "${HOME}/.bashrc"
+fi
 
-source "${HOME}/.bashrc"
-
-# Set output path for era5t dataset
+echo "Setting ERA5T dataset path in recipes/regrid.yaml"
 era5t_path="$SCRATCH/datasets/era5t.zarr"
-sed -i -e "s/\(dataset:\s\).*/\1$era5t_path" recipes/regrid.yaml
+sed -e "s|\(dataset:\s\).*|\1$era5t_path|" recipes/regrid_template.yaml >recipes/regrid.yaml
 
 uv sync --frozen
