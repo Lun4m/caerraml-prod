3e5f868644bcbe8aa2d3c1d3b7f09a321797eea2
Manuel Carrer <manuel.carrer@zohomail.eu>
2026-08-25 16:34:22 +0200

    Fix wrong modules and missing hidden package

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=inference
 #SBATCH --output=logs/%x.out
-##SBATCH --qos=ng
-#SBATCH --qos=dg
+#SBATCH --qos=ng
 #SBATCH --ntasks=1
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=8
@@ -13,10 +12,12 @@ set -e
 date=$1
 
 module purge
-module load uv
+module load prgenv/gnu
+module load gcc/11.5.0
 module load cuda/13.0
-module load ecmwf-toolbox
+module load ecmwf-toolbox/2026.04.0.0
 module load python3/3.12.11
+module load uv
 
 uv run --frozen caerra_tu_ml prepare "$date"
 uv run --frozen caerra_tu_ml run "$date"
