0e6c7cc51ce94ddf4d0b1dbfb10bc95f000c6eb9
Manuel Carrer <manuel.carrer@zohomail.eu>
2026-08-26 16:28:59 +0200

    Get rid of unset hack by not loading cuda module

--- infer.sh
+++ infer.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=00:30:00
+#SBATCH --time=03:00:00
 #SBATCH --hint=nomultithread
 
 set -e
@@ -14,12 +14,8 @@ date=$1
 module purge
 module load prgenv/gnu
 module load gcc/11.5.0
-module load cuda/13.0
 module load ecmwf-toolbox/2026.04.0.0
 module load python3/3.12.11
 module load uv
-
-# NOTE: required to avoid cublas runtime errors
-unset LD_LIBRARY_PATH
 
 uv run --frozen caerra-tu-ml "$date"
