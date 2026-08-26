126487189a08599cd3e59480c4e6836812842734
Manuel Carrer <manuel.carrer@zohomail.eu>
2026-08-26 16:31:14 +0200

    Remove setting input regrid dataset during install

--- install.sh
+++ install.sh
@@ -14,12 +14,7 @@ fi
 
 case $cluster in
 AC)
-    echo "Setting ERA5T dataset path in recipes/regrid.yaml"
-    base_dir="packages/prepare/recipes"
-    out_path="$SCRATCH/datasets/era5t.zarr"
-    sed -e "s|\(dataset:\s\).*|\1$out_path|" $base_dir/regrid_template.yaml >$base_dir/regrid.yaml
-
-    # Only need to prepare on AC
+    # Only need to prepare datasets on AC
     cd packages/prepare
     uv sync --frozen
     ;;
